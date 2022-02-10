031b971a08220f0b217c5071583cf94cefd8e360
Bastiche <bastien.velitchkine@live.fr>
2022-02-10 13:17:57 +0100

    [+] Added epoch progress printer

---
+++ printer/progress.py
@@ -0,0 +1,14 @@
+def print_progress(epoch, total_number_epochs, loss):
+    """
+    A function that prints the current epoch and the average loss on a given image during this epoch
+    """
+    if epoch == total_number_epochs:
+        print(f'Step: {epoch}/{total_number_epochs} - Loss: {loss: .4f}')
+    else:
+        print(f'Step: {epoch}/{total_number_epochs} - Loss: {loss: .4f}', end="\r")
+
+if __name__ == "__main__":
+    print_progress(1, 4, 0.0567)
+    print_progress(2, 4, 0.0378)
+    print_progress(4, 4, 0.0019)
+    
