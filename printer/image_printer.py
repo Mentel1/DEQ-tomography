08fb367a383d33e4d300469ff99d6ea041a5e0b4
Bastiche <bastien.velitchkine@live.fr>
2022-02-10 11:42:51 +0100

    [~] Fixed grayscale printer that already takes gray imgs as inputs

--- printer/image_printer.py
+++ printer/image_printer.py
@@ -1,14 +1,17 @@
 import torch
 import matplotlib.pyplot as plt
 
-def print_grayscale(loader, image_index):
+def print_grayscale(loader, image_index, input_image=True):
     """
     A function that prints a grayscale image of our liking in a data set of gray images
     """
     dataiter = iter(loader)
-    images, _ = next(dataiter)
 
-    gray_image = images[image_index].to(torch.uint8)[0, :, :]
-    npimg = gray_image.numpy()
-    plt.imshow(npimg, cmap='gray', vmin=0, vmax=255)
-    plt.show();
+    if input_image:
+        images, _ = next(dataiter)
+    else:
+        _, images = next(dataiter)
+
+    gray_image = images[image_index].numpy()
+    plt.imshow(gray_image, cmap='gray')
+    plt.show()
