81fa8be6acaaf2534973cbbd71ddad3a1b3af7ec
Bastiche <bastien.velitchkine@live.fr>
2022-03-06 14:18:24 +0100

    [*] Updated main app to take tomosipo operator into account

--- apps/train.py
+++ apps/train.py
@@ -11,6 +11,7 @@ from trainer.image_reconstruction import reconstruction_epoch
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 torch.manual_seed(0)
 batch_size = 5
+tomosipo = False
 
 # Data loading
 train_dataset = Tomography('./data/')
@@ -18,8 +19,13 @@ test_dataset = Tomography('./data/','test')
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
 
-false_operator = torch.randn((110, 512))
-f = ForwardBackwardLayer(false_operator, .01)
+if tomosipo:
+    from operators.radon import RadonTransform
+    operator = RadonTransform(image_batch_size=batch_size).operator()
+else:
+    operator = torch.randn((110, 512))
+
+f = ForwardBackwardLayer(operator, .01, tomosipo=tomosipo)
 model = ReconstructionDEQ(f, anderson, tol=1e-2, max_iter=25, m=5).to(device)
 opt = optim.Adam(model.parameters(), lr=1e-3)
 
@@ -28,6 +34,6 @@ scheduler = optim.lr_scheduler.CosineAnnealingLR(opt, MAX_EPOCH * len(train_load
 num_epochs = 2
 
 for i in range(num_epochs):
-    epoch_train_loss = reconstruction_epoch(train_loader, model, false_operator, device, opt, scheduler)
-    epoch_test_loss = reconstruction_epoch(test_loader, model, false_operator, device)
+    epoch_train_loss = reconstruction_epoch(train_loader, model, operator, device, opt, scheduler)
+    epoch_test_loss = reconstruction_epoch(test_loader, model, operator, device)
     print_progress(i, num_epochs, epoch_test_loss, training=False)
