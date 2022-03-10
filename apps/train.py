5c85fcab25aa867ca6f424581ea2a2e2d4eff26d
Bastiche <bastien.velitchkine@live.fr>
2022-03-10 12:25:59 +0100

    [*] Changed default parameters so solver does not raise errors

--- apps/train.py
+++ apps/train.py
@@ -10,8 +10,10 @@ from trainer.image_reconstruction import reconstruction_epoch
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 torch.manual_seed(0)
+
 batch_size = 5
-tomosipo = False
+channels = 1
+tomosipo = True # Change this to False if no tomosipo environment available
 
 # Data loading
 train_dataset = Tomography('./data/')
@@ -21,13 +23,15 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch
 
 if tomosipo:
     from operators.radon import RadonTransform
-    operator = RadonTransform(image_batch_size=batch_size).operator()
+    operator = RadonTransform(channels=channels)
 else:
     operator = torch.randn((110, 512))
 
 f = ForwardBackwardLayer(operator, .01, tomosipo=tomosipo)
+
 # Play with anderson's hyperparameter in case solver raises singular matrix errors
-model = ReconstructionDEQ(f, anderson, tol=1e-2, max_iter=25, m=5).to(device)
+model = ReconstructionDEQ(f, anderson, tol=1e-2, max_iter=25, beta=1., lam=1e-2).to(device)
+
 opt = optim.Adam(model.parameters(), lr=1e-3)
 
 MAX_EPOCH = 50
