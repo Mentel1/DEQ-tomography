3ab88f8c93346e491aed82c38b9fd4890d91b34d
lilasainero <lila.sainero@student-cs.fr>
2022-04-05 15:37:55 +0200

    update train

--- apps/train.py
+++ apps/train.py
@@ -1,8 +1,9 @@
 import torch
 import torch.optim as optim
+import matplotlib.pyplot as plt
 
 import sys
-sys.path.insert(0, 'C:/Users/Lila/Documents/Centrale/3A/Projet Deep Equilibrium/Git projet/DEQ-tomography')
+# sys.path.insert(0, 'C:/Users/Lila/Documents/Centrale/3A/Projet Deep Equilibrium/Git projet/DEQ-tomography')
 
 from loader.DataLoaders import Tomography
 from printer.progress import print_progress
@@ -16,7 +17,7 @@ from torch.utils.tensorboard import SummaryWriter
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 torch.manual_seed(0)
 
-batch_size = 5
+batch_size = 25
 channels = 1
 tomosipo = False # Change this to False if no tomosipo environment available
 
@@ -33,14 +34,23 @@ else:
     operator = torch.randn((110, 512))
 
 f = ForwardBackwardLayer(operator, .01, tomosipo=tomosipo)
+tol=1e-2
+max_iter=50
+beta=0.1
+lam=1e-2
+lr=1e-4
 # Play with anderson's hyperparameter in case solver raises singular matrix errors
-model = ReconstructionDEQ(f, anderson, tol=1e-2, max_iter=25, beta=1., lam=1e-2).to(device)
-opt = optim.Adam(model.parameters(), lr=1e-3)
+model = ReconstructionDEQ(f, anderson, tol=tol, max_iter=max_iter, beta=beta, lam=lam).to(device)
+opt = optim.Adam(model.parameters(), lr=lr)
+
 MAX_EPOCH = 50
 scheduler = optim.lr_scheduler.CosineAnnealingLR(opt, MAX_EPOCH * len(train_loader), eta_min=1e-6)
-num_epochs = 15
+num_epochs = 10
 
 writer = SummaryWriter()
+
+train_loss = []
+test_loss = []
 
 for i in range(num_epochs):
 
@@ -49,7 +59,19 @@ for i in range(num_epochs):
     writer.add_scalar("Loss/test", epoch_test_loss, i)
     writer.add_scalar("Loss/train", epoch_train_loss, i)
     writer.add_scalars(f'Loss/both', {'test': epoch_test_loss,'train': epoch_train_loss,}, i)
+    train_loss.append(epoch_train_loss)
+    test_loss.append(epoch_test_loss)
     print_progress(i, num_epochs, epoch_test_loss, training=False)
 
 writer.flush()
 # To see results, run 'tensorboard --logdir=runs' and go to the provided url (or to http://localhost:6006/)
+
+torch.save(model.state_dict(), 'model_weights.pth')
+    
+plt.figure()
+plt.plot(train_loss[1:], label="train")
+plt.plot(test_loss[1:], 'r', label="test")
+plt.title(f"tol={tol}, max_iter={max_iter}, beta={beta}, lam={lam}, MAX_EPOCH={MAX_EPOCH}, num_epochs={num_epochs}, lr={lr}")
+plt.legend()
+plt.semilogy()
+plt.savefig(f"tol={tol}, max_iter={max_iter}, beta={beta}, lam={lam}, MAX_EPOCH={MAX_EPOCH}, num_epochs={num_epochs}, lr={lr}.png")
