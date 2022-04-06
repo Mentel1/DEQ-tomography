c58d7460913d818b13da1dff15a21d69bee7f099
lilasainero <lila.sainero@student-cs.fr>
2022-04-06 23:34:29 +0200

    Comment "plt.show()" in reconstruction_printer in case there isn't a GUI backend

--- printer/reconstruction_printer.py
+++ printer/reconstruction_printer.py
@@ -7,5 +7,6 @@ def reconstruction_printer(x_result, img, i):
     fig, axes = plt.subplots(1,2)
     axes[0].imshow(np.flipud(x_result.detach().cpu().numpy().reshape(512, 512)), cmap='gray')
     axes[1].imshow(img.detach().cpu().numpy().reshape(512, 512), cmap='gray')
-    plt.show()
+    # Un-comment line if you work on a GUI-backend 
+    # plt.show()
     plt.savefig(f"results_{i}.jpg")
