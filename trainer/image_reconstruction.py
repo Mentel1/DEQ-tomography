8b42610d4cea898f320d46dcaa20f92621a7c058
lilasainero <lila.sainero@student-cs.fr>
2022-04-06 23:43:27 +0200

    Scalling loss

--- trainer/image_reconstruction.py
+++ trainer/image_reconstruction.py
@@ -6,6 +6,7 @@ def reconstruction_epoch(loader, model, operator, device, opt=None, lr_scheduler
     The trainer to call at each epoch
     """
     total_loss = 0.
+    N = 0
     _ = model.eval() if opt is None else model.train()
     for sinograms, images in loader:
         images, sinograms = images.to(device), sinograms.to(device)
@@ -23,5 +24,6 @@ def reconstruction_epoch(loader, model, operator, device, opt=None, lr_scheduler
             lr_scheduler.step()
 
         total_loss += loss.item()
+        N += 1
 
-    return total_loss / len(loader.dataset)
+    return total_loss / N
