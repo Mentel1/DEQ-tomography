ee9f3a7310b93e21a6c7d177bd51dde00c3995f0
lilasainero <lila.sainero@student-cs.fr>
2022-04-06 23:31:45 +0200

    Loss graphs to dedicated folder

--- printer/loss_printer.py
+++ printer/loss_printer.py
@@ -8,4 +8,4 @@ def loss_printer(train_loss, test_loss, tol, max_iter, beta, lam, MAX_EPOCH, num
     plt.title(f"tol={tol}, max_iter={max_iter}, beta={beta}, lam={lam}, MAX_EPOCH={MAX_EPOCH}, num_epochs={num_epochs}, lr_model={lr_model}, lr_fb={lr_fb}")
     plt.legend()
     plt.semilogy()
-    plt.savefig(f"tol={tol}_max_iter={max_iter}_beta={beta}_lam={lam}_MAX_EPOCH={MAX_EPOCH}_num_epochs={num_epochs}_lrmodel={lr_model}_lrfb={lr_fb}.png")
+    plt.savefig(f"results/tol={tol}_max_iter={max_iter}_beta={beta}_lam={lam}_MAX_EPOCH={MAX_EPOCH}_num_epochs={num_epochs}_lrmodel={lr_model}_lrfb={lr_fb}.png")
